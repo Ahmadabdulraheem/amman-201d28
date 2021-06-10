7c1d17b3dc86a0308a823cc4fcb0ad1d66118356
Ahmadabdulraheem <ahmad.abdulrahem@outlook.com>
2021-06-10 17:39:54 +0300

    create branch prob 4 and solve the problem

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,8 +8,9 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b,c) { //eslint-disable-line
   let total = a+b;
+
   return [total, `The sum of ${a} and ${b} is ${total}.`];
 }
 // console.log( sum(4, 11));
@@ -77,12 +78,13 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let total = testArray[0]+testArray[1]+testArray[2];
+return [total, `2,3,4 was passed in as an array of numbers, and ${total} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
