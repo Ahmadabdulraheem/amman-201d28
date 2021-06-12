90752a99dacd9af2909f05af329000a7d68df2a7
Ahmadabdulraheem <ahmad.abdulrahem@outlook.com>
2021-06-12 18:15:51 +0300

    solving problem 4

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -53,14 +53,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  let total = a+b+c;
-  let product =a*b*c;
-  return [total, product, `${a} and ${b} and ${c} sum to ${total}.`,  `The product of ${a} and ${b} and ${c} is ${product}.`];
+
   
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
- testSumAndMultiply(4,7,5);
+ //testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -78,8 +76,12 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-  let total = testArray[0]+testArray[1]+testArray[2];
-return [total, `2,3,4 was passed in as an array of numbers, and ${total} is their sum.`];
+  let total=0;
+
+  for (let i = 0; i<sumArr.length;i++){
+    total = sum(total,sumArr[i])[0];
+  }
+  return [total, `${sumArr} was passed in as an array of numbers, and ${total} is their sum.`]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
